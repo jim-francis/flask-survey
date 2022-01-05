ee427d64bad0340a9dd90b1c1f17beba38c51742
Jimmy Francis <jimmyfrancis@protonmail.com>
2022-01-04 21:03:03 -0500

    Implement Question page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "nge4ev"
@@ -11,6 +11,15 @@ responses = []
 
 @app.route('/')
 def show_homepage():
-    survey_title = satisfaction_survey.title
-    survey_instructions = satisfaction_survey.instructions
+    survey_title = survey.title
+    survey_instructions = survey.instructions
     return render_template('home.html', survey_title=survey_title, survey_instructions=survey_instructions)
+
+@app.route('/question/<question_id>')
+def ask_question(question_id):
+    # question_to_ask = satisfaction_survey.questions
+    question_id = len(responses)
+    question = survey.questions[question_id]
+    return render_template(
+        "question.html", question_num=question_id, question=question)
+    
